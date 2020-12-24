35e06e0149b433f99475fa403dd6c4e5957f9e57
Hidetoshi Kawaguchi <kawa.hide39@gmail.com>
2020-12-24 21:25:31 +0900

    Spartan: edited samples.

--- spartan/sample/command.py
+++ spartan/sample/command.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 import sys, json, random
-from time import sleep
-
 if __name__ == '__main__':
     param = json.loads(sys.argv[1])
-    sleep(random.randint(0, 3))
-    print(param, type(param))
-
-
-
-
-
+    v = random.random()
+    for _ in range(int(v * (3 * 10**8))):
+        pass
+    print(param, v)
